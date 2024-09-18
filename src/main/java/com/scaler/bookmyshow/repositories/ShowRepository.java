59b0da5997e36afa9bacaebccf900eeaadb44964
ayush diwan <ayushdiwan98@gmail.com>
2024-09-19 01:48:54 +0530

    Book My Show 19-09-2024

--- src/main/java/com/scaler/bookmyshow/repositories/ShowRepository.java
+++ src/main/java/com/scaler/bookmyshow/repositories/ShowRepository.java
@@ -1,8 +1,11 @@
 package com.scaler.bookmyshow.repositories;
 
 import com.scaler.bookmyshow.Models.Shows;
-
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.Optional;
+
 public interface ShowRepository extends JpaRepository<Shows, Long> {
+    @Override
+    Optional<Shows> findById(Long aLong);
 }
--- src/main/java/com/scaler/bookmyshow/services/BookingService.java
+++ src/main/java/com/scaler/bookmyshow/services/BookingService.java
@@ -1,36 +1,65 @@
 package com.scaler.bookmyshow.services;
 
-import com.scaler.bookmyshow.Models.Booking;
-
+import com.scaler.bookmyshow.Models.*;
+import com.scaler.bookmyshow.repositories.ShowRepository;
+import com.scaler.bookmyshow.repositories.ShowSeatRepository;
 import com.scaler.bookmyshow.repositories.UserRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Isolation;
 import org.springframework.transaction.annotation.Transactional;
-import com.scaler.bookmyshow.Models.User;
-import java.util.Optional;
 
-
+import java.time.Duration;
+import java.util.ArrayList;
+import java.util.Date;
 import java.util.List;
+import java.util.Optional;
 
 @Service
 public class BookingService {
 
     private UserRepository userRepository;
+    private ShowRepository showRepository;
+    private ShowSeatRepository showSeatRepository;
 
     @Autowired
-    BookingService(UserRepository userRepository) {
+    BookingService(UserRepository userRepository, ShowRepository showRepository, ShowSeatRepository showSeatRepository) {
         this.userRepository = userRepository;
+        this.showRepository = showRepository;
+        this.showSeatRepository = showSeatRepository;
     }
 
     @Transactional(isolation = Isolation.SERIALIZABLE)
     public Booking bookMovie(
         Long userId,
-        List<Long> seatId,
+        List<Long> showSeatIds,
         Long showId
      ){
 
         Optional<User> userOptional = userRepository.findById(userId);
+
+        if(userOptional.isEmpty()){
+            throw new RuntimeException("No such user found!");
+        }
+        User BookedBy = userOptional.get();
+        Optional<Shows> showOptional = showRepository.findById(showId);
+        if(showOptional.isEmpty()){
+            throw new RuntimeException("No show like that!");
+        }
+        Shows bookedShow = showOptional.get();
+        List<ShowSeat> showSeats = showSeatRepository.findAllById(showSeatIds);
+        for(ShowSeat showSeat :showSeats){
+            if(!(showSeat.getShowSeatStatus().equals(ShowSeatStatus.AVAILABLE) ||
+                    (showSeat.getShowSeatStatus().equals(ShowSeatStatus.BLOCKED) &&
+                            Duration.between(showSeat.getLockedAt().toInstant(), new Date().toInstant()).toMinutes() < 15))){
+                throw new RuntimeException("Selected seats are not available!");
+            }
+        }
+        List<ShowSeat> savedShowSeats = new ArrayList<>();
+        for(ShowSeat showSeat : showSeats){
+            showSeat.setShowSeatStatus(ShowSeatStatus.BLOCKED);
+            savedShowSeats.add(showSeatRepository.save(showSeat));
+        }
          return null;
     }
 }
